b1bbbedc742f73974c8a200060d0214f323f2d9d
fin170 <timmytom444@gmail.com>
2021-11-18 14:40:16 +0000

    menu level with giant power up

---
+++ major project/Assets/Scripts/Destructable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("car"))
+        {
+            if (GaintMode.gaintMode == true)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
