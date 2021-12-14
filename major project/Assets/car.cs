25c9e283f13deb3f07f86f14e391150bb3acbf63
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2021-12-14 17:09:17 +0000

    different car scripts

---
+++ major project/Assets/car.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalinput;
+    private float verticalinput;
+    private float currentsteerangle;
+    private float currentbreakforce;
+    private bool isBreaking;
+
+    [SerializeField] private float motorforce;
+    [SerializeField] private float breakforce;
+    [SerializeField] private float maxsteerangle;
+
+    [SerializeField] private WheelCollider frontleftwheel;
+    [SerializeField] private WheelCollider frontrightwheel;
+    [SerializeField] private WheelCollider backleftwheel;
+    [SerializeField] private WheelCollider backrightwheel;
+
+    [SerializeField] private Transform frontleftwheeltransform;
+    [SerializeField] private Transform frontrightwheeltransform;
+    [SerializeField] private Transform backleftwheeltransform;
+    [SerializeField] private Transform backrightwheeltransform;
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        Handlesteering();
+        Updatewheels();
+    }
+    private void GetInput()
+    {
+        horizontalinput = Input.GetAxis(HORIZONTAL);
+        verticalinput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        frontleftwheel.motorTorque = verticalinput * motorforce;
+        frontrightwheel.motorTorque = verticalinput * motorforce;
+        currentbreakforce = isBreaking ? breakforce : 0f;
+         ApplyBreaking();
+        
+    }
+    private void ApplyBreaking()
+    {
+        frontrightwheel.brakeTorque = currentbreakforce;
+        frontleftwheel.brakeTorque = currentbreakforce;
+        backrightwheel.brakeTorque = currentbreakforce;
+        backleftwheel.brakeTorque = currentbreakforce;
+    }
+    private void Handlesteering()
+    {
+        currentsteerangle = maxsteerangle * horizontalinput;
+        frontleftwheel.steerAngle = currentsteerangle;
+        frontrightwheel.steerAngle = currentsteerangle;
+    }
+    private void Updatewheels()
+    {
+        updateSinglewheel(frontleftwheel, frontleftwheeltransform);
+        updateSinglewheel(frontrightwheel, frontrightwheeltransform);
+        updateSinglewheel(backleftwheel, backleftwheeltransform);
+        updateSinglewheel(backrightwheel, backrightwheeltransform);
+    }
+    private void updateSinglewheel(WheelCollider wheelcollider, Transform wheeltransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+         wheelcollider.GetWorldPose(out pos, out rot);
+        wheeltransform.rotation = rot;
+        wheeltransform.position = pos;   
+    }
+}
