95fd3498c6aebc4289b2a706ee333128c645adeb
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2022-02-12 19:49:34 +0000

    added wheel script
    wheel script clamps and goes back into original position when not turning

--- major project/Assets/roataewheel.cs
+++ major project/Assets/roataewheel.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class roataewheel : MonoBehaviour
 {
     // public GameObject steeringwheel;
-    private float wheelrotation;
+    public float wheelrotation;
     public float speed;
-    public float wheel;
+    public Quaternion TO;
     
-  
+    //  public float wheel;
+
+
+    private void Start()
+    {
+        // TO = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
 
     public void FixedUpdate()
     {
+
+
         wheelrotation += Input.GetAxis("Horizontal")*speed;
         wheelrotation = Mathf.Clamp(wheelrotation, -120, 120);
         transform.Rotate(0, 0, -wheelrotation, Space.Self);
-        //  Vector3 current_rotation = transform.rotation.eulerAngles;
-        //   current_rotation.z =  Mathf.Clamp(wheel, -120, 120);
-        //   transform.rotation = Quaternion.Euler(-current_rotation);
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, -wheelrotation);
-
-        if(Input.GetAxis("Horizontal") == 0)
+          transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, -wheelrotation);
+       // transform.rotation = new Vector3(-wheelrotation);
+      
+    }
+    public void Update()
+    {
+        if (Input.GetAxis("Horizontal") == 0)
         {
-         //   Quaternion.Lerp();
+         //roatates the wheel back to orignal place
+            wheelrotation = Mathf.Lerp(wheelrotation, 0f, 1f*Time.deltaTime);
         }
     }
 }
