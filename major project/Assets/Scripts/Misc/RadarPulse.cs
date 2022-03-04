56bd95aaa3697b24007a73c664eb77f1af078eb3
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2022-03-04 17:02:29 +0000

    radar

--- major project/Assets/Scripts/Misc/RadarPulse.cs
+++ major project/Assets/Scripts/Misc/RadarPulse.cs
@@ -1,45 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class RadarPulse : MonoBehaviour
 {
-    private Transform pulseTransform;
-    private float range;
-    public float maxRange = 300f;
-    public float rangeSpeed = 150f;
-    private List<Collider2D> alreadyPingedCollider;
-    // Start is called before the first frame update
-    void Start()
+    public Transform sweepTransfrom;
+    public float rotateSpeed = 180f;
+
+
+    public void Start()
     {
-        pulseTransform = transform.Find("Pulse");
-        alreadyPingedCollider = new List<Collider2D>();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        range += rangeSpeed * Time.deltaTime;
-        if(range > maxRange)
-        {
-            range = 0f;
-            alreadyPingedCollider.Clear();
-        }
-
-        pulseTransform.localScale = new Vector3(range, range);
-
-        RaycastHit2D[] raycastHit = Physics2D.CircleCastAll(transform.position, range / 2f, Vector2.zero);
-        foreach (RaycastHit2D hit2D in raycastHit)
-        {
-            if (hit2D.collider != null)
-            {
-
-                if (!alreadyPingedCollider.Contains(hit2D.collider))
-                {
-                    alreadyPingedCollider.Add(hit2D.collider);
-                    Debug.Log("Hit");
-                }
-            }
-        }
+        sweepTransfrom.eulerAngles -= new Vector3(0, 0, rotateSpeed * Time.deltaTime);
     }
 }
