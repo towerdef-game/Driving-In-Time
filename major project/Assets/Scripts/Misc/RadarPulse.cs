8247523abb467d4332003b641c9a2746d3391edb
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2022-03-16 10:52:51 +0000

    b

--- major project/Assets/Scripts/Misc/RadarPulse.cs
+++ major project/Assets/Scripts/Misc/RadarPulse.cs
@@ -8,100 +8,96 @@ public class RadarPulse : MonoBehaviour
 {
     public Transform sweepTransfrom;
     public float rotateSpeed = 180f;
-    public float radarDistance = 20, blipSize = 15;
-    public bool usePlayerDirection = true;
+    public float distanceOfRadar = 20;
+    public float sizeOdBlips = 15;
+    public bool playerDirection = true;
     public Transform player;
-    public GameObject blipRedPrefab, blipGreenPrefab;
-    public string redBlipTag = "Enemy", greenBlipTag = "NPC";
+    public GameObject civilianPrefab;
+    public GameObject enemyPrefab;
+    public string greenTag = "NPC";
+    public string redTag = "Enemy";
 
-    private float radarWidth, radarHeight, blipWidth, blipHeight;
+    private float widthOfTheRadar, heightOfTheRadar, widthOfTheBlip, heightOfTheBlip;
 
     void Start()
     {
-        radarWidth = GetComponent<RectTransform>().rect.width;
-        radarHeight = GetComponent<RectTransform>().rect.height;
-        blipHeight = radarHeight * blipSize / 100;
-        blipWidth = radarWidth * blipSize / 100;
+        widthOfTheRadar = GetComponent<RectTransform>().rect.width;
+        heightOfTheRadar = GetComponent<RectTransform>().rect.height;
+        heightOfTheBlip = heightOfTheRadar * sizeOdBlips / 100;
+        widthOfTheBlip = widthOfTheRadar * sizeOdBlips / 100;
     }
 
     void Update()
     {
         sweepTransfrom.eulerAngles -= new Vector3(0, 0, rotateSpeed * Time.deltaTime);
-        RemoveAllBlips();
-        DisplayBlips(redBlipTag, blipRedPrefab);
-        DisplayBlips(greenBlipTag, blipGreenPrefab);
+        RemoveBlips();
+        DisplayBlips(redTag, enemyPrefab);
+        DisplayBlips(greenTag, civilianPrefab);
     }
 
-    private void DisplayBlips(string tag, GameObject prefabBlip)
+    private void DisplayBlips(string tag, GameObject blipPrefab)
     {
-        Vector3 playerPos = player.position;
+        Vector3 playerPosition = player.position;
         GameObject[] targets = GameObject.FindGameObjectsWithTag(tag);
 
         foreach (GameObject target in targets)
         {
-            Vector3 targetPos = target.transform.position;
-            float distanceToTarget = Vector3.Distance(targetPos, playerPos);
+            Vector3 targetPosition = target.transform.position;
+            float distToTarget = Vector3.Distance(targetPosition, playerPosition);
 
-            if (distanceToTarget <= radarDistance)
+            if (distToTarget <= distanceOfRadar)
             {
 
-                Vector3 normalisedTargetPosition = NormalisedPosition(playerPos, targetPos);
-                Vector2 blipPosition = CalculateBlipPosition(normalisedTargetPosition);
-                DrawBlip(blipPosition, prefabBlip);
+                Vector3 normalisedTargetPosition = NormalisedPos(playerPosition, targetPosition);
+                Vector2 positionOfBlip = CalculateAllBlipsPositions(normalisedTargetPosition);
+                DrawBlipOnRadar(positionOfBlip, blipPrefab);
             }
         }
     }
 
-    private void RemoveAllBlips()
+    private void RemoveBlips()
     {
-        GameObject[] blips = GameObject.FindGameObjectsWithTag("RadarBlip");
-        foreach (GameObject blip in blips)
+        GameObject[] allBlips = GameObject.FindGameObjectsWithTag("RadarBlip");
+        foreach (GameObject blip in allBlips)
             Destroy(blip);
     }
 
-    private Vector3 NormalisedPosition(Vector3 playerPos, Vector3 targetPos)
+    private Vector3 NormalisedPos(Vector3 positionOfPlayer, Vector3 positionOfTarget)
     {
-        float normalisedTargetX = (targetPos.x - playerPos.x) / radarDistance;
-        float normalisedTargetZ = (targetPos.z - playerPos.z) / radarDistance;
+        float normalisedTargetXPosition = (positionOfTarget.x - positionOfPlayer.x) / distanceOfRadar;
+        float normalisedTargetZPosition = (positionOfTarget.z - positionOfPlayer.z) / distanceOfRadar;
 
-        return new Vector3(normalisedTargetX, 0, normalisedTargetZ);
+        return new Vector3(normalisedTargetXPosition, 0, normalisedTargetZPosition);
     }
 
-    private Vector2 CalculateBlipPosition(Vector3 targetPos)
+    private Vector2 CalculateAllBlipsPositions(Vector3 targetPositions)
     {
-        // find the angle from the player to the target.
-        float angleToTarget = Mathf.Atan2(targetPos.x, targetPos.z) * Mathf.Rad2Deg;
+        float targetAngle = Mathf.Atan2(targetPositions.x, targetPositions.z) * Mathf.Rad2Deg;
 
-        // The direction the player is facing.
-        float anglePlayer = usePlayerDirection ? player.eulerAngles.y : 0;
+        float playerAngle = playerDirection ? player.eulerAngles.y : 0;
 
-        // Subtract the player angle, to get the relative angle to the object. Subtract 90
-        // so 0 degrees (the same direction as the player) is Up.
-        float angleRadarDegrees = angleToTarget - anglePlayer - 90;
+        float degreeOfRadarAngle = targetAngle - playerAngle - 90;
 
-        // Calculate the xy position given the angle and the distance.
-        float normalisedDistanceToTarget = targetPos.magnitude;
-        float angleRadians = angleRadarDegrees * Mathf.Deg2Rad;
-        float blipX = normalisedDistanceToTarget * Mathf.Cos(angleRadians);
-        float blipY = normalisedDistanceToTarget * Mathf.Sin(angleRadians);
+        float normalisedDistanceToTarget = targetPositions.magnitude;
+        float radiansAngle = degreeOfRadarAngle * Mathf.Deg2Rad;
+        float blipPositionX = normalisedDistanceToTarget * Mathf.Cos(radiansAngle);
+        float blipPositionY = normalisedDistanceToTarget * Mathf.Sin(radiansAngle);
+   
+        blipPositionX *= widthOfTheRadar * .5f;
+        blipPositionY *= heightOfTheRadar * .5f;
 
-        // Scale the blip position according to the radar size.
-        blipX *= radarWidth * .5f;
-        blipY *= radarHeight * .5f;
+        blipPositionX += (widthOfTheRadar * .5f) - widthOfTheBlip * .5f;
+        blipPositionY += (heightOfTheRadar * .5f) - heightOfTheBlip * .5f;
 
-        // Offset the blip position relative to the radar center
-        blipX += (radarWidth * .5f) - blipWidth * .5f;
-        blipY += (radarHeight * .5f) - blipHeight * .5f;
-
-        return new Vector2(blipX, blipY);
+        return new Vector2(blipPositionX, blipPositionY);
     }
 
-    private void DrawBlip(Vector2 pos, GameObject blipPrefab)
+    private void DrawBlipOnRadar(Vector2 position, GameObject blipPrefab)
     {
-        GameObject blip = (GameObject)Instantiate(blipPrefab);
-        blip.transform.SetParent(transform);
-        RectTransform rt = blip.GetComponent<RectTransform>();
-        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, pos.x, blipWidth);
-        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, pos.y, blipHeight);
+        GameObject blipAllPrefabs = (GameObject)Instantiate(blipPrefab);
+        blipAllPrefabs.transform.SetParent(transform);
+        RectTransform rectTransform = blipAllPrefabs.GetComponent<RectTransform>();
+        rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, position.x, widthOfTheBlip);
+        rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, position.y, heightOfTheBlip);
     }
 }
