2a4ff09173d78c0d9f12969a3fecc8f0491b19fa
fin170 <timmytom444@gmail.com>
2022-03-19 01:38:42 +0000

    enemy now ray casts to check if player is in view

--- major project/Assets/Scripts/NPC/Enemy/EnemyAi.cs
+++ major project/Assets/Scripts/NPC/Enemy/EnemyAi.cs
@@ -41,27 +41,30 @@ public class EnemyAi : MonoBehaviour
         Vector3 minDist = target.position + target.position/10 - transform.position;
         shotTime -= 1 * Time.deltaTime;
 
-        if ((shootRadius > Mathf.Abs(dist.x) && Mathf.Abs(dist.z) < shootRadius) && shotTime < 0)
+        //if ((shootRadius > Mathf.Abs(dist.x) && Mathf.Abs(dist.z) < shootRadius) && shotTime < 0)
+        //{
+        agent.speed = speed;
+        agent.SetDestination(target.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, target.position-transform.position, out hit) && hit.transform.tag == "Player")
         {
-
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, target.position, out hit))
+            Debug.Log(hit.collider.name);
+            transform.LookAt(target.position);
+            if (shotTime <= 0)
             {
-                transform.LookAt(target.position);
                 Instantiate(enemyBullet, transform.position + new Vector3(0, 0, 2), Quaternion.identity);
                 shotTime = 5f;
-                agent.SetDestination(transform.position);
-                enemySounds.SetParameter("shoot", 1);
             }
-            else enemySounds.SetParameter("shoot", 0);
-        } 
-        if ( Mathf.Abs(dist.x)> shootRadius || Mathf.Abs(dist.z) > shootRadius)
-        {
-           //agent.stoppingDistance use this 
-            agent.speed = speed;
-            agent.SetDestination(target.position);
-        }
-
+            agent.SetDestination(transform.position);
+            enemySounds.SetParameter("shoot", 1);
+        } else enemySounds.SetParameter("shoot", 0);
+            //} 
+            //if (Mathf.Abs(dist.x) > shootRadius || Mathf.Abs(dist.z) > shootRadius)
+            //{
+                //agent.stoppingDistance use this 
+                
+            //}
+        
        
         //    Debug.Log("cant run?" + Mathf.Abs(dist.x) + " is bigger than"+shootRadius + " and some how" + Mathf.Abs(dist.z) + " is smaller than" + shootRadius);
         
