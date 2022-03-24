821c479130f97c1f3cf7211451f07a72478ebb4e
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2022-03-24 14:53:03 +0000

    respawn

---
+++ major project/Assets/Scripts/car/RespawnPlayer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+   // public Transform player;
+   // public Transform spawnPoint;
+
+
+    public float remainingTime = 10;
+    public bool timerIsOn = false;
+    public float remainingTimeTillSpawn = 3;
+    public bool timerIsOnToSpawn = false;
+    public Transform playerPosition;
+    public Vector3 respawnPosition;
+    public bool grounded = false;
+    private void Start()
+    {
+       
+        timerIsOn = true;
+        timerIsOnToSpawn = false;
+    }
+    void Update()
+    {
+        if (timerIsOn)// timer is on and grounded
+        {
+            if (remainingTime > 0)
+            {
+                remainingTime -= Time.deltaTime;
+               
+            }
+            else
+            {
+                remainingTime = 15;
+                // take the players vector 3 position
+               
+               respawnPosition = playerPosition.transform.position;
+
+            }
+        }
+
+        if (timerIsOnToSpawn)
+        {
+            if (remainingTimeTillSpawn > 0)
+            {
+                remainingTimeTillSpawn -= Time.deltaTime;
+
+            }
+            else
+            {
+                
+                remainingTimeTillSpawn = 3;
+                playerPosition.position = respawnPosition;
+                timerIsOnToSpawn = false;
+            }
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.gameObject.tag == "DeathField")
+        {
+            //Debug.Log("hit trigger");
+
+            timerIsOnToSpawn = true;
+            
+        }
+    }
+}
